3f157b60f682bbacacd1b1e3527e1a1b01028a7d
charliesoko <56273909+charliesoko@users.noreply.github.com>
2020-10-22 16:28:47 -0500

    Version 1.2
    Added wave spawning system to manage enemies.

---
+++ Flight to the Deep/Assets/Scripts/WaveManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] wavesArray;
+
+    [SerializeField]
+    private int waveNumber;
+
+    private bool isWaveActive;
+
+    void Start()
+    {
+        waveNumber = 0;
+        isWaveActive = false;
+    }
+
+
+    void Update()
+    {
+        if (!isWaveActive)
+        {
+            Instantiate(wavesArray[waveNumber], transform.position, transform.rotation);
+            isWaveActive = true;
+        }
+
+        if (allDead())
+        {
+            isWaveActive = false;
+            waveNumber++;
+        }
+    }
+
+    private bool allDead()
+    {
+        GameObject enemy = GameObject.FindGameObjectWithTag("Enemy");
+
+        if (enemy == null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
